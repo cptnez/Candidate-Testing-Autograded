3c0d4e1138e2c3a455f372428cfabac9c0951dcd
Grant Tracey <gtracey24@gmail.com>
2024-01-10 14:13:28 -0600

    Commit after passing tests for Part on and Part 2

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,34 +3,38 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? " ];
+let correctAnswers = ['Sally Ride', 'true', '40', 'Trajectory', '3'];
 let candidateAnswers;
 
 
-function askForName() {
+function askForName(candidateName) {
   // TODO 1.1b: Ask for candidate's name //
-
+ console.log("What is your name?: ");
 }
 
-function askQuestion() {
+function askQuestion(candidateAnswer) {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+console.log(question);
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
+  if (candidateAnswer = "Sally Ride") {
+    console.log("That's right! ");
+  } else {
+    console.log("Wrong! ");
+  }
 
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
@@ -42,7 +46,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log();
+   console.log("Greetings " + candidateName + " !");
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
