c3515a034919cdec8950984887c4d8a8234dde7a
Grant Tracey <gtracey24@gmail.com>
2024-01-16 15:29:37 -0600

    Completed part two of candidate testing. Each questiong is asked one at a time.

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,9 +11,9 @@ let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? " ];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3" ];
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -23,14 +23,22 @@ candidateName = input.question("What is your name?: ");
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-console.log(question)
-candidateAnswer = input.question();
+console.log(questions[0])
+candidateAnswers = input.question();
+console.log(questions[1])
+candidateAnswers = input.question();
+console.log(questions[2])
+candidateAnswers = input.question();
+console.log(questions[3])
+candidateAnswers = input.question();
+console.log(questions[4])
+candidateAnswers = input.question();
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (candidateAnswer == correctAnswer) {
+if (candidateAnswers == correctAnswer) {
   console.log("Correct!")
 } else {
   console.log("Wrong!");
